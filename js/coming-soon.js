a8b141802645a259fffcc49a7099465a2f9e9007
Nadia Presberg <npresberg@gmail.com>
2021-04-29 17:07:00 -0300

    derecha

--- js/coming-soon.js
+++ js/coming-soon.js
@@ -1,6 +1,8 @@
-(function($) {
+(function ($) {
   "use strict"; // Start of use strict
 
   // No JS
+
+  // cuando responde el formulario: gtag_report_conversion()
 
 })(jQuery); // End of use strict
